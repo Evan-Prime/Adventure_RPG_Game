78146118152d7c19f62e796c248f48d31de2a7d0
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-10-08 22:56:12 -0300

    12% done the tutorial

--- Assets/Testing/T_Scripts/GameController02.cs
+++ Assets/Testing/T_Scripts/GameController02.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class GameController02 : MonoBehaviour
 {
+
+    public GameObject[] gameCameras;
+
+    private int gameCameraIndex = 0;
+    
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World!");
+       FocusOnCamera (gameCameraIndex);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown (0))
+        {
+            ChangeCamera (1);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            ChangeCamera(-1);
+        }
+    }
+
+    void FocusOnCamera (int index)
+    {
+        for (int i = 0; i < gameCameras.Length; i++)
+        {
+            gameCameras[i].SetActive (i == index);
+        }
+    }
+
+    void ChangeCamera (int direction)
+    {
+        gameCameraIndex += direction;
+
+        if (gameCameraIndex >= gameCameras.Length)
+        {
+            gameCameraIndex = 0;
+        }
+
+        if (gameCameraIndex < 0)
+        {
+            gameCameraIndex = gameCameras.Length - 1;
+        }
+
+        FocusOnCamera (gameCameraIndex);
     }
 }
---
+++ Assets/Testing/T_Scripts/Player.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 3.5f;
+    public float rotatingSpeed = 40f;
+    public float jumpingForce = 10f;
+    
+    private bool canJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey("right"))
+        {
+            transform.RotateAround(transform.position, Vector3.up, rotatingSpeed * Time.deltaTime);
+            Debug.Log("Turn Right");
+        }
+
+        if (Input.GetKey("left"))
+        {
+            transform.RotateAround(transform.position, Vector3.up, -rotatingSpeed * Time.deltaTime);
+            Debug.Log("Move Left");
+        }
+
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+            Debug.Log("Move Up");
+        }
+
+        if (Input.GetKey("down"))
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+            Debug.Log("Move Down");
+        }
+
+        if (Input.GetKeyDown("space") && canJump)
+        {
+            canJump = false;
+            GetComponent<Rigidbody>().AddForce(0, jumpingForce, 0);
+            Debug.Log("Jump");
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "Floor")
+        {
+            Debug.Log("Hit the floor");
+            canJump = true;
+        }
+    }
+}
