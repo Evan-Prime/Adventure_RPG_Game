6f4578edff726f06615f71e1977dfc04f7bc5b1d
Evan MacDonald <evan.t.macdonald01@gmail.com>
2022-11-15 08:50:38 -0400

    added to shooting enemy code

--- Assets/Project/Scripts/Enemies/ShootingEnemy.cs
+++ Assets/Project/Scripts/Enemies/ShootingEnemy.cs
@@ -6,16 +6,29 @@ public class ShootingEnemy : Enemy
 {
 
     public GameObject model;
+    public float timeToRotate = 2f;
+
+    private int targetAngle;
+    private float rotationTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rotationTimer = timeToRotate;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rotationTimer -= Time.deltaTime;
+        if (rotationTimer <= 0)
+        {
+            rotationTimer = timeToRotate;
+
+            targetAngle += 90;
+        }
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, targetAngle, 0), Time.deltaTime);
+
     }
 }
